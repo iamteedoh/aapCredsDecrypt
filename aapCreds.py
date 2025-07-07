eeddb5c73d0c94beb0d7a92f5f02e55322ca6fec
Tito Valentin <7767846+iamteedoh@users.noreply.github.com>
2025-07-07 15:24:09 -0400

    Add GPL info

--- aapCreds.py
+++ aapCreds.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python
+
+## Author: Tito Valentin
+
+## Copyright (C) 2025 TEEDOH LABS INC copying@teedohlabs.com
+##
+## This file is part of aapCredsDecrypt.
+##
+## aapCredsDecrypt is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## aapCredsDecrypt is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with aapJobCleanupHistory. If not, see <https://www.gnu.org/licenses/>.
+
 """
 AWX/AAP Credential Export/Import Management Command
 
